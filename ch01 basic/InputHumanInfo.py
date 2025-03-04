7b9196b3d65f6d259ad712ac3b42c1a6ec00e481
Bluebull <woolra112@gmail.com>
2025-03-04 12:32:06 +0900

    second commit

---
+++ ch01 basic/InputHumanInfo.py
@@ -0,0 +1,18 @@
+print('이름 입력 : ', end='') #end = '' 옵션은 엔터키를 누르지 않겠다.
+name = input()
+
+print('나이 입력 : ', end='')
+age = input() #input함수는 숫자로 인식한다.
+age = int(age) #파이썬에서의 정수형 형변환
+
+print('키 입력 : ', end='')
+height = float(input()) #실수 타입 변환. 한줄로도 가능
+
+print('이름 : %s' % (name))
+print('나이 : %d' % (age))
+print('나이 : %.2f' % (height)) #포맷팅시, 표시된 자릿수까지 유지하고 다음 자리 숫자를 기준으로 반올림
+
+
+#f-String을 사용한 문자열 포맷팅
+sentense = f"이름 : {name}, 나이 : {age}, 키 : {height:.1f}cm"
+print(sentense)
--- ch01 basic/PercentFormatting01.py
+++ ch01 basic/PercentFormatting01.py
@@ -1,1 +1,34 @@
+
 name = "김철수" #샵은 주석입니다.
+age = 30
+height =175.3456789 ; address = '마포'
+
+# % 포맷 코드
+print('이름 : %s' % (name))
+print('나이 : %d' % (age))
+print('키01 : %.3f' % (height))
+print('키02 : [%10.4f]' % (height))
+print('이름 : %s' % (name))
+print('주소 : %s' % (address))
+
+message = '이름은 %s이고, 나이는 %d입니다.'
+print(message % (name, age)) #여러 개의 변수 사용시 소괄호 사용
+
+su = 2
+bread = '단팥빵'
+print('나는 배고파서 %s %d개를 먹었습니다.' % (bread, su))
+
+su01 = 3
+su02 = 5
+print('%d 더하기 %d는 %d입니다.' % (su01, su02, (su01+su02)))
+
+#f-String을 사용한 문자열 포맷팅
+sentense = f"이름 : {name}, 나이 : {age}, 키 : {height:.1f}cm"
+print(sentense)
+
+hello = '안녕'
+message = '[%10s]' % hello # 오른쪽 정렬 (공백 앞쪽)
+print(message)
+
+message = '[%-10s]' % hello # 왼쪽 정렬 (공백 뒤쪽)
+print(message)
